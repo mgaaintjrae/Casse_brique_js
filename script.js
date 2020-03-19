9030b538dd968d5440acdc28033750c3046b2825
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-03-19 10:04:24 +0100

    Paddle move

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ console.log("coucou");
 // 1.Créer l'élément canvas et l'afficher
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+let ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+
+// 4.Contrôles clavier
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+      rightPressed = true;
+  } else if (e.keyCode == 37) {
+      leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+      rightPressed = false;
+  } else if (e.keyCode == 37) {
+      leftPressed = false;
+  }
+}
 // Carré rouge
 // ctx.beginPath();
 // ctx.rect(20, 40, 50, 50);
@@ -33,24 +59,58 @@ let dy = -2;
 // 2.Bouger la boule
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
-function draw() { 
+// ----- construction du paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();    
+    drawBall();
+    drawPaddle();
+
+    // 3.Rebondir sur les murs
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = - dx;
+    }
+    if(y + dy < ballRadius) {
+      dy = -dy;
+    }
+    else if(y +dy > canvas.height-ballRadius) {
+      if(x > paddleX && x < paddleX + paddleWidth) {
+        dy = -dy;
+      } else {
+        clearInterval(game);
+        alert("GAME OVER");
+        document.location.reload();
+      }
+    }    
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
     x += dx; // direction
     y += dy;
 }
 
-setInterval(draw, 10);
+let game = setInterval(draw, 10);
 
 
-// 3.Rebondir sur les murs
-// 4.Contrôles clavier
+
+
 // 5.Jeu terminé
 // 6.Construire le mur de briques
 // 7.Détection des collisions
