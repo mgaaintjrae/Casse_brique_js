f829121f6715cbecbf325180ce5cd36792a27525
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-03-22 15:43:48 +0100

    Detection collision

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ for (let c = 0; c < brickColumnCount; c++) {
     for (let r = 0; r < brickRowCount; r++) {
         bricks[c][r] = {
             x: 0,
-            y: 0
+            y: 0,
+            status: 1
         };
     }
 }
@@ -74,6 +75,20 @@ function keyUpHandler(e) {
 // ctx.stroke();
 // ctx.closePath();
 
+// 7.Détection des collisions
+function collisionDetection() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            let b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = - dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
 
 // 2.Bouger la boule
 function drawBall() {
@@ -95,19 +110,19 @@ function drawPaddle() {
 
 // ----- construction du mur de briques
 function drawBricks() {
-  for(let c=0; c<brickColumnCount; c++) {
-    for(let r=0; r<brickRowCount; r++) {
-      let brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
-      let brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath;
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            let brickX = (r * (brickWidth + brickPadding)) + brickOffsetLeft;
+            let brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath;
+        }
     }
-  }
 }
 
 function draw() {
@@ -115,6 +130,7 @@ function draw() {
     drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
 
     // 3.Rebondir sur les murs
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
@@ -146,7 +162,6 @@ function draw() {
 let game = setInterval(draw, 10);
 
 
-// 7.Détection des collisions
 // 8.Afficher le score et gagner
 // 9.Contrôles souris
 // 10.Finir
